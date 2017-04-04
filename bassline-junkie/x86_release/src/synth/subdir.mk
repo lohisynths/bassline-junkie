d75887ec7e7fe65947566efd200b06c0320fdef8
lohisynths <lohisynths@gmail.com>
2017-04-04 18:09:22 +0200

    .gitignore update

---
+++ bassline-junkie/x86_release/src/synth/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/synth/AudioDevice.cpp \
+../src/synth/MoogFilter.cpp \
+../src/synth/bassline-junkie.cpp \
+../src/synth/cpucounter.cpp \
+../src/synth/synth.cpp \
+../src/synth/wavwriter.cpp 
+
+OBJS += \
+./src/synth/AudioDevice.o \
+./src/synth/MoogFilter.o \
+./src/synth/bassline-junkie.o \
+./src/synth/cpucounter.o \
+./src/synth/synth.o \
+./src/synth/wavwriter.o 
+
+CPP_DEPS += \
+./src/synth/AudioDevice.d \
+./src/synth/MoogFilter.d \
+./src/synth/bassline-junkie.d \
+./src/synth/cpucounter.d \
+./src/synth/synth.d \
+./src/synth/wavwriter.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/synth/%.o: ../src/synth/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	clang++ -std=c++0x -I../../Plot -I../../stk/include -I../../Plot -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ bassline-junkie/x86_tests/src/tests/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/tests/bench.cpp 
+
+OBJS += \
+./src/tests/bench.o 
+
+CPP_DEPS += \
+./src/tests/bench.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/tests/%.o: ../src/tests/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	clang++ -std=c++1y -I"/home/alax/git/bassline-junkie/Plot" -I../../benchmark/include -I../../stk/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
