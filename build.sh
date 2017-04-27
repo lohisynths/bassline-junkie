a11d0dad693e6d685933400ce55858e98c0f90ea
lohisynths <lohisynths@gmail.com>
2017-04-28 00:44:39 +0200

    init/build.sh modified - cross compiling by default

--- build.sh
+++ build.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 set -e
 
-#cd stk
-#autoreconf
-#./configure CXX="clang++" CC="clang" --enable-debug --with-alsa --disable-shared --enable-static
-#./configure --enable-debug --with-alsa --disable-shared --enable-static
+# http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BGreen='\033[1;32m'
+NC='\033[0m'
 
 
 make clean -C stk
 make -j4 lib -C stk
 
+#make -j4 clean -C bassline-junkie/x86_debug
+#make -j4 clean -C bassline-junkie/x86_release
+#make -j4 clean -C bassline-junkie/x86_tests
 
-make -j4 clean -C bassline-junkie/x86_debug
-make -j4 clean -C bassline-junkie/x86_release
-make -j4 clean -C bassline-junkie/x86_tests
+#make -j4 -C bassline-junkie/x86_debug
+#make -j4 -C bassline-junkie/x86_release
+#make -j4 -C bassline-junkie/x86_tests
 
+make -j4 clean -C bassline-junkie/arm_debug
 
-make -j4 -C bassline-junkie/x86_debug
-make -j4 -C bassline-junkie/x86_release
-make -j4 -C bassline-junkie/x86_tests
-
-
-
-echo "TADAAAAAAA  "
+echo -e ${BGreen} 'build done' ${NC}
--- init.sh
+++ init.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+set -e 
+cross_compiler=arm-linux-gnueabihf-g++
+
+# http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BGreen='\033[1;32m'
+NC='\033[0m'
+
+# http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+command -v $cross_compiler >/dev/null 2>&1 || { echo -e >&2 ${RED} $cross_compiler "is requied but it's not installed.  Aborting." ${NC}; exit 1; }
+type $cross_compiler >/dev/null 2>&1 || { echo >&2 $cross_compiler "is requied but it's not installed.  Aborting."; exit 1; }
+hash $cross_compiler 2>/dev/null || { echo >&2 $cross_compiler "is requied but it's not installed.  Aborting."; exit 1; }
+
+echo -e ${GREEN} 'cross-compiler' $cross_compiler 'found' ${NC}
+
+echo -e ${GREEN} 'submodule init' ${NC}
 git submodule init
 git submodule update
 
 cd stk
 
-
+echo -e ${GREEN} 'updating stk subrepo' ${NC}
 git checkout master
+
+echo -e ${GREEN} 'configuring stk subrepo' ${NC}
 autoreconf
 #./configure --enable-debug --with-alsa --disable-shared --enable-static
-./configure --with-alsa --disable-shared --enable-static
+#./configure --with-alsa --disable-shared --enable-static
+./configure --host=arm-linux-gnueabihf --build=x86_64-linux-gnu --enable-debug --with-alsa --disable-shared --enable-static
+
+
+echo -e ${BGreen} 'done' ${NC}
