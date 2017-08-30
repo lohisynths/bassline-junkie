8cfe15e138a2694c6f2779a534852ddb6d942cbd
lohisynths <lohisynths@gmail.com>
2017-08-30 21:06:39 +0200

    stk release build recipe update

--- bassline-junkie/x86_release/src/synth/dsp/subdir.mk
+++ bassline-junkie/x86_release/src/synth/dsp/subdir.mk
@@ -4,13 +4,22 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/synth/dsp/MoogFilter.cpp 
+../src/synth/dsp/Lfo.cpp \
+../src/synth/dsp/MoogFilter.cpp \
+../src/synth/dsp/Osc.cpp \
+../src/synth/dsp/VAStateVariableFilter.cpp 
 
 OBJS += \
-./src/synth/dsp/MoogFilter.o 
+./src/synth/dsp/Lfo.o \
+./src/synth/dsp/MoogFilter.o \
+./src/synth/dsp/Osc.o \
+./src/synth/dsp/VAStateVariableFilter.o 
 
 CPP_DEPS += \
-./src/synth/dsp/MoogFilter.d 
+./src/synth/dsp/Lfo.d \
+./src/synth/dsp/MoogFilter.d \
+./src/synth/dsp/Osc.d \
+./src/synth/dsp/VAStateVariableFilter.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
