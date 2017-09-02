5e2a86562872c28fb717c2c2d562c565bfbda35d
lohisynths <lohisynths@gmail.com>
2017-09-02 15:52:40 +0200

    build scripts updated

--- build.sh
+++ build.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-set -e
+set -e 
 
-# http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BGreen='\033[1;32m'
-NC='\033[0m'
+main() {
+	if [ "$#" -ne 1 ]
+	then
+	  echo "Usage: ..."
+	  exit 1
+	fi
+
+	for i in "$@" ; do
+	    if [[ $i == "arm" ]] ; then
+		echo "Is set and compiling!"
+		make clean -C stk
+		make -j4 lib -C stk
+
+		make -j4 clean -C bassline-junkie/arm_debug
+		make -j4 -C bassline-junkie/arm_debug
+
+		break
 
 
-make clean -C stk
-make -j4 lib -C stk
+	    elif [[ $i == "x86" ]] ; then
+		echo "Is set and compiling!"
+		make clean -C stk
+		make -j4 lib -C stk
+
+		make -j4 clean -C bassline-junkie/x86_debug
+		make -j4 -C bassline-junkie/x86_debug
+		break
+	    fi
+	done
+
+echo -e ${BGreen} 'build done' ${NC}
+
+}
+
+
+main "$@"
+
 
 #make -j4 clean -C bassline-junkie/x86_debug
 #make -j4 clean -C bassline-junkie/x86_release
@@ -19,8 +47,6 @@ make -j4 lib -C stk
 #make -j4 -C bassline-junkie/x86_release
 #make -j4 -C bassline-junkie/x86_tests
 
-make -j4 clean -C bassline-junkie/arm_debug
 
-make -j4 -C bassline-junkie/arm_debug
 
-echo -e ${BGreen} 'build done' ${NC}
+
