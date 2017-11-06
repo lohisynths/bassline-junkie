5807862544c68b12396d0b35659550d76320f3d1
lohisynths <lohisynths@gmail.com>
2017-11-06 12:11:05 +0100

    [pd] updated, forward ard serial to hw loopback serial ACO0 ACM1

--- bassline-junkie/src/synth/utils/SerialReceiver.cpp
+++ bassline-junkie/src/synth/utils/SerialReceiver.cpp
@@ -15,7 +15,7 @@
 
 SerialReceiver::SerialReceiver() : running(true)
 {
-	const char *portname = "/dev/ttyACM0";
+	const char *portname = "/dev/ttyACM1";
 
 	fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
 	if (fd < 0) {
