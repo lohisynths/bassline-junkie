76dd33d7495265d9e19e52907fb2a9f18b5340ef
lohisynths <lohisynths@gmail.com>
2018-07-12 20:52:44 +0200

    cmake try

--- build.sh
+++ build.sh
@@ -1,52 +1,78 @@
 #!/bin/bash
-set -e 
+# Ubuntu supports multiple versions of clang to be installed at the same time.
+# The tests need to determin the clang binary before calling cmake
 
-main() {
-	if [ "$#" -ne 1 ]
-	then
-	  echo "Usage: ..."
-	  exit 1
-	fi
+CMAKE_PRIVATE_FLAGS=
 
-	for i in "$@" ; do
-	    if [[ $i == "arm" ]] ; then
-		echo "Is set and compiling!"
-		make clean -C stk
-		make -j4 lib -C stk
+TOOLCHAIN_FILE="cmake/arm-linux-toolchain.cmake"
 
-		make -j4 clean -C bassline-junkie/arm_debug
-		make -j4 -C bassline-junkie/arm_debug
-
-		break
-
-
-	    elif [[ $i == "x86" ]] ; then
-		echo "Is set and compiling!"
-		make clean -C stk
-		make -j4 lib -C stk
-
-		make -j4 clean -C bassline-junkie/x86_debug
-		make -j4 -C bassline-junkie/x86_debug
-		break
-	    fi
-	done
-
-echo -e ${BGreen} 'build done' ${NC}
-
+function abort {
+    echo "usage:"
+    echo "       build.sh { clang | gcc } {Release | Debug}"
+    exit
 }
 
+if [ -z "$1" ] || [ -z "$2" ] ; then
+    abort
+fi
 
-main "$@"
+if [ "$1" = "clang" ]; then
+    CC_BIN=`which clang`
+    CXX_BIN=`which clang++`
+    if [ -z $CC_BIN ]; then
+        clang_ver=`dpkg --get-selections | grep clang | grep -v -m1 libclang | cut -f1 | cut -d '-' -f2`
+        CC_BIN="clang-$clang_ver"
+        CXX_BIN="clang++-$clang_ver"
+    fi
+    echo "Will use clang [$CC_BIN] and clang++ [$CXX_BIN]"
+    CMAKE_PRIVATE_FLAGS="$CMAKE_PRIVATE_FLAGS -DCMAKE_CXX_COMPILER=$CXX_BIN"
+    echo $CMAKE_PRIVATE_FLAGS
+elif [  "$1" = "gcc" ] ; then
+    CC_BIN=`which gcc`
+    CXX_BIN=`which g++`
+    if [ -z $CC_BIN ]; then
+        clang_ver=`dpkg --get-selections | grep gcc | grep -v -m1 libclang | cut -f1 | cut -d '-' -f2`
+        CC_BIN="clang-$clang_ver"
+        CXX_BIN="clang++-$clang_ver"
+    fi
+    echo "Will use clang [$CC_BIN] and clang++ [$CXX_BIN]"
+    CMAKE_PRIVATE_FLAGS=$CMAKE_PRIVATE_FLAGS -DCMAKE_CXX_COMPILER=$CXX_BIN
+    echo $CMAKE_PRIVATE_FLAGS
+    echo "Will use gcc [$CC_BIN] and g++ [$CXX_BIN]"
+elif [  "$1" = "arm" ] ; then
+    echo "arm arm arm arm"
+    CMAKE_PRIVATE_FLAGS="$CMAKE_PRIVATE_FLAGS -DCMAKE_CXX_COMPILER=$CXX_BIN"
+else
+    echo "Wrong compiler selected"
+    abort
+fi
+
+mkdir -p build && cd build
+
+echo $CMAKE_PRIVATE_FLAGS
 
 
-#make -j4 clean -C bassline-junkie/x86_debug
-#make -j4 clean -C bassline-junkie/x86_release
-#make -j4 clean -C bassline-junkie/x86_tests
+if [ "$2" = "Release" ]; then
+    cmake $CMAKE_PRIVATE_FLAGS -DCMAKE_BUILD_TYPE=Release ../
+elif [  "$2" = "Debug" ] ; then
+    cmake $CMAKE_PRIVATE_FLAGS -DCMAKE_BUILD_TYPE=Debug ../
+else
+    echo "Wrong build configuration selected"
+    abort
+fi
 
-#make -j4 -C bassline-junkie/x86_debug
-#make -j4 -C bassline-junkie/x86_release
-#make -j4 -C bassline-junkie/x86_tests
 
 
+if [ "$2" = "Release" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release $CMAKE_PRIVATE_FLAGS ../
+elif [  "$2" = "Debug" ] ; then
+    cmake -DCMAKE_BUILD_TYPE=Debug $CMAKE_PRIVATE_FLAGS ../
+else
+    echo "Wrong build configuration selected"
+    abort
+fi
+
+
+make -j 8
 
 
