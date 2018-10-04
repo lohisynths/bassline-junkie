754fd07ee271242126d20ca099a2772af007a127
lohisynths <lohisynths@gmail.com>
2018-10-04 03:01:33 +0200

    rapik

--- bassline-junkie/src/tests/osc_benchmark/osc_bench.cpp
+++ bassline-junkie/src/tests/osc_benchmark/osc_bench.cpp
@@ -1,4 +1,6 @@
 #include <benchmark/benchmark.h>
+#include <fstream>
+#define DUMP_RESULTS 1
 
 #include "BlitSaw.h"
 #include "Blit.h"
@@ -7,19 +9,30 @@
 #include "dsp/PolyBLEP/PolyBLEP.h"
 #include "dsp/stmlib_polybleep.h"
 
+#define ARR_SIZE 1025
+
 void BlitSaw_tick(benchmark::State& state)
 {
     stk::Stk::setSampleRate(96000);
 	stk::BlitSaw osc;
-    stk::StkFloat  output[512];
+    stk::StkFloat  output[ARR_SIZE];
 
 	while (state.KeepRunning()) {
-		for(int i=0; i<512; i++) {
+		for(int i=0; i<ARR_SIZE; i++) {
 	        osc.setFrequency(state.range(0));
 	        output[i] = osc.tick();
 		}
 	    benchmark::DoNotOptimize(output);
 	}
+#if DUMP_RESULTS
+    std::string filename("stk_BlitSaw");
+    filename += "_data_";
+    filename += std::to_string(state.range(0));
+    filename += ".bin";
+
+    std::ofstream outfile_m(filename, std::ios::binary);
+            outfile_m.write((const char*)output, sizeof(output));
+#endif
 }
 BENCHMARK(BlitSaw_tick)->Arg(110)->Arg(440)->Arg(880);
 
@@ -28,15 +41,24 @@ void PolyBleepOsc_tick(benchmark::State& state)
     PolyBLEPOscillator osc;
     osc.setSampleRate(96000);
     osc.setMode(Oscillator::OSCILLATOR_MODE_SAW);
-    stk::StkFloat output[512];
+    stk::StkFloat output[ARR_SIZE];
 
     while (state.KeepRunning()) {
-        for(int i=0; i<512; i++) {
+        for(int i=0; i<ARR_SIZE; i++) {
             osc.setFrequency(state.range(0));
             output[i] = osc.nextSample();
         }
         benchmark::DoNotOptimize(output);
     }
+#if DUMP_RESULTS
+    std::string filename("PolyBLEPOscillator");
+    filename += "_data_";
+    filename += std::to_string(state.range(0));
+    filename += ".bin";
+
+    std::ofstream outfile_m(filename, std::ios::binary);
+            outfile_m.write((const char*)output, sizeof(output));
+#endif
 }
 BENCHMARK(PolyBleepOsc_tick)->Arg(110)->Arg(440)->Arg(880);
 
@@ -44,31 +66,49 @@ void StmPolyBleepOsc_tick(benchmark::State& state)
 {
     stmlib::Oscillator osc;
     osc.Init();
-    stk::StkFloat output[512];
+    stk::StkFloat output[ARR_SIZE];
 
     while (state.KeepRunning()) {
-        for(int i=0; i<512; i++) {
+        for(int i=0; i<ARR_SIZE; i++) {
             float out;
             osc.Render<true>((state.range(0) / 96000.), (state.range(0) / 2. / 96000.), 0.5f, 0.0f, &out, 1);
             output[i] = out;
         }
         benchmark::DoNotOptimize(output);
     }
+#if DUMP_RESULTS
+    std::string filename("stmlib_Oscillator");
+    filename += "_data_";
+    filename += std::to_string(state.range(0));
+    filename += ".bin";
+
+    std::ofstream outfile_m(filename, std::ios::binary);
+            outfile_m.write((const char*)output, sizeof(output));
+#endif
 }
 BENCHMARK(StmPolyBleepOsc_tick)->Arg(110)->Arg(440)->Arg(880);
 
 void PolyBleep_tick(benchmark::State& state)
 {
     static PolyBLEP osc(96000, PolyBLEP::SAWTOOTH, 440);
-    stk::StkFloat output[512];
+    stk::StkFloat output[ARR_SIZE];
 
     while (state.KeepRunning()) {
-        for(int i=0; i<512; i++) {
+        for(int i=0; i<ARR_SIZE; i++) {
             osc.setFrequency(state.range(0));
             output[i] = osc.getAndInc();
             benchmark::DoNotOptimize(output[i]);
         }
     }
+#if DUMP_RESULTS
+    std::string filename("PolyBLEP");
+    filename += "_data_";
+    filename += std::to_string(state.range(0));
+    filename += ".bin";
+
+    std::ofstream outfile_m(filename, std::ios::binary);
+            outfile_m.write((const char*)output, sizeof(output));
+#endif
 }
 BENCHMARK(PolyBleep_tick)->Arg(110)->Arg(440)->Arg(880);
 
