00ac3a52992951d02e05ad215838203ba5615ce5
Adam Cwiek <napent@gmail.com>
2026-04-07 21:43:08 +0200

    Reduce synth buffer size to 256 samples

--- bassline-junkie/src/synth/config.h
+++ bassline-junkie/src/synth/config.h
@@ -29,7 +29,7 @@ const size_t first_cpu=1;
 const size_t max_cores=3;
 
 const size_t format_bits = 32; //snd_pcm_format_width(*m_format);
-const size_t buffer_size = 1024;
+const size_t buffer_size = 256;
 const size_t sample_rate = 96000;
 
 
