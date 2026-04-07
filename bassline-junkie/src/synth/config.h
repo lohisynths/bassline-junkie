79079773f041efdd3d2d01820e0f3876eb1800a2
Adam Cwiek <lohisynths@gmail.com>
2026-04-07 21:13:45 +0200

    Increase synth buffer size and sample rate
    - Bump audio buffer size to 1024
    - Raise sample rate to 96 kHz

--- bassline-junkie/src/synth/config.h
+++ bassline-junkie/src/synth/config.h
@@ -29,8 +29,8 @@ const size_t first_cpu=1;
 const size_t max_cores=3;
 
 const size_t format_bits = 32; //snd_pcm_format_width(*m_format);
-const size_t buffer_size = 512;
-const size_t sample_rate = 44100;
+const size_t buffer_size = 1024;
+const size_t sample_rate = 96000;
 
 
 #endif /* SRC_CONFIG_H_ */
